36fd4e1999121294645fa60cf3c70661d3378f39
VenRen <fandeus.ren@outlook.com>
2021-07-08 22:28:28 +0400

    修改代码到range

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,32 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
 
-    if (healthPoints == 100) {
-        println(name + "is in excellent condition!")
-    } else if (healthPoints >= 90) {
-        println(name + " has a few scratches.")
-    } else if (healthPoints >= 75) {
-        println(name + " has some minor wounds.")
-    } else if (healthPoints >= 15){
-        println(name + " looks pretty hurt.")
-    }else {
-        println(name + "is in awful condition!")
+    //Aura
+    val auraVisible = isBlessed && healthPoints>50 || isImmortal
+
+    val auraColor = if (auraVisible) "Green" else "None"
+    println(auraColor)
+
+    val healthStatus = if (healthPoints == 100){
+        "is in excellent condition!"
+    }else if (healthPoints in 90..99){
+        "has a few scratches."
+    }else if (healthPoints in 75..89){
+        if (isBlessed){
+            "has some minor wounds but is healing quite quickly!"
+        }else{
+            "has some minor wounds."
+        }
+    }else if (healthPoints in 15..74){
+        "looks pretty hurt."
+    }else{
+        "is in awful condition!"
     }
+
+    //Player status
+    println("$name $healthStatus")
 
 }
