6b2f3e1682d7cc5c0a4d461d4ce3dece745e14b4
Wen <developer.work@outlook.com>
2022-11-09 12:00:20 +0400

    重新创建项目

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,7 @@
 fun main(args: Array<String>) {
-    val name = "Madrigal"
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
+    println("Hello World!")
 
-    //Aura
-    val auraVisible = isBlessed && healthPoints>50 || isImmortal
-
-    val auraColor = if (auraVisible) "Green" else "None"
-    println(auraColor)
-
-    val healthStatus = if (healthPoints == 100){
-        "is in excellent condition!"
-    }else if (healthPoints in 90..99){
-        "has a few scratches."
-    }else if (healthPoints in 75..89){
-        if (isBlessed){
-            "has some minor wounds but is healing quite quickly!"
-        }else{
-            "has some minor wounds."
-        }
-    }else if (healthPoints in 15..74){
-        "looks pretty hurt."
-    }else{
-        "is in awful condition!"
-    }
-
-    //Player status
-    println("$name $healthStatus")
-
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
 }
