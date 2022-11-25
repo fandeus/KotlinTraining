c73e5648a3e46a283ffdc8f3f9b063dee697c672
Wen <developer.work@outlook.com>
2022-11-25 13:31:46 +0400

    字符串的比较

--- src/main/kotlin/nyetHack/Tavern.kt
+++ src/main/kotlin/nyetHack/Tavern.kt
@@ -22,5 +22,22 @@ class Tavern {
         val (type, name, price) = meanData.split(',')
         val message = "Madrigal buys a $name($type) for $price."
         println(message)
+        val phrase = "Ah, delicious $name"
+        println("Madrigal exclaims:${toDragonSpeak(phrase)}")
+
     }
+
+    private fun toDragonSpeak(phrase: String) =
+        phrase.replace(Regex("[aeiou]")) {
+            when (it.value) {
+                "a" -> "4"
+                "e" -> "3"
+                "i" -> "1"
+                "o" -> "0"
+                "u" -> "|_|"
+                else -> it.value
+            }
+        }
+
+
 }
